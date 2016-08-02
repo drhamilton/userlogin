54db441f01b8402f9edef9a5a4295df2f2704664
Dillon H <dillonrhamilton@gmail.com>
2016-08-01 21:44:27 -0400

    Update DB

--- config.php
+++ config.php
@@ -4,7 +4,6 @@ function initDB(){
     $db = new SQLite3(DB_NAME);
     $db->busyTimeout(5000);
     $db->exec('PRAGMA journal_mode = wal;');
-
     $db->exec("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, email TEXT UNIQUE, name TEXT, password TEXT)");
 
     return $db;
--- index.php
+++ index.php
@@ -1,41 +1,6 @@
 <?php
-    class DB extends SQLite3
-    {
-        function __construct()
-        {
-            $this->open('db/database.sqlite');
-        }
-
-        function
-    }
-    $db = new DB();
-
-    if (!$db){
-        echo $db->lastErrorMsg();
-    }
-    else {
-        echo "Opened DB";
-    }
+    require './config.php';
+    require './router.php';
 
 
-
-//    $db = new SQLite3('db/database.sqlite');
-//    $db->exec('CREATE TABLE foo (bar STRING)');
-//    $db->exec("INSERT INTO foo (bar) VALUES ('This is a test')");
-//    $result = $db->query('SELECT bar FROM foo');
-//    var_dump($result->fetchArray());
-
-    $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
-
-    switch  ($path) {
-        case '/login':
-            echo 'Logging you in!';
-            break;
-        case '/register':
-            echo 'Hey, we are registering now!';
-            break;
-        default:
-            echo 'Guess I will just render the homepage. You requested: ' . $path . '.' ;
-            break;
-    }
 
